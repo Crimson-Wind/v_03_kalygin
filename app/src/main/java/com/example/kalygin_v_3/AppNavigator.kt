c06cb5f13ec3eee5612f3a82c1d8767a6a2ebcdb
Alex <lesha.kalugin.blood@gmail.com>
2025-01-30 15:20:22 +0700

    gthd

--- app/src/main/java/com/example/kalygin_v_3/AppNavigator.kt
+++ app/src/main/java/com/example/kalygin_v_3/AppNavigator.kt
@@ -8,7 +8,6 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun AppNavigator(){
     val navController = rememberNavController()
-
     NavHost(navController = navController, startDestination = "login") {
         composable("login") { LoginScreen(navController) }
         composable("personal_area") { PersonalAreaScreen(navController) }
